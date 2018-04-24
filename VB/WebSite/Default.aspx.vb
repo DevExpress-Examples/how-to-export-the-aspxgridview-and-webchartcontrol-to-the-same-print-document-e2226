cc176417ee1f231c5b8849c7ba9ad1d2e527548f
DevExpressExampleBot <fakeEmail>
2018-04-24 14:31:22 +0300

    Source auto update [skip ci]

--- VB/WebSite/Default.aspx.vb
+++ VB/WebSite/Default.aspx.vb
@@ -1,5 +1,4 @@
-Imports Microsoft.VisualBasic
-Imports System
+﻿Imports System
 Imports System.IO
 Imports DevExpress.XtraCharts
 Imports DevExpress.XtraCharts.Native
@@ -7,37 +6,38 @@ Imports DevExpress.XtraPrinting
 Imports DevExpress.XtraPrintingLinks
 
 Partial Public Class _Default
-	Inherits System.Web.UI.Page
-	Protected Sub Page_Load(ByVal sender As Object, ByVal e As EventArgs)
-		Chart.SeriesDataMember = "CategoryName"
-		Chart.SeriesTemplate.ArgumentDataMember = "ProductSales"
-		Chart.SeriesTemplate.ValueDataMembers.AddRange(New String() { "ProductSales" })
-		Chart.SeriesTemplate.View = New StackedBarSeriesView()
-	End Sub
-	Protected Sub ExportButton_Click(ByVal sender As Object, ByVal e As EventArgs)
-		Dim ps As New PrintingSystem()
+    Inherits System.Web.UI.Page
 
-		Dim link1 As New PrintableComponentLink(ps)
-		link1.Component = GridExporter
+    Protected Sub Page_Load(ByVal sender As Object, ByVal e As EventArgs)
+        Chart.SeriesDataMember = "CategoryName"
+        Chart.SeriesTemplate.ArgumentDataMember = "ProductSales"
+        Chart.SeriesTemplate.ValueDataMembers.AddRange(New String() { "ProductSales" })
+        Chart.SeriesTemplate.View = New StackedBarSeriesView()
+    End Sub
+    Protected Sub ExportButton_Click(ByVal sender As Object, ByVal e As EventArgs)
+        Dim ps As New PrintingSystem()
 
-		Dim link2 As New PrintableComponentLink(ps)
-		Chart.DataBind()
-		link2.Component = (CType(Chart, IChartContainer)).Chart
+        Dim link1 As New PrintableComponentLink(ps)
+        link1.Component = GridExporter
 
-		Dim compositeLink As New CompositeLink(ps)
-		compositeLink.Links.AddRange(New Object() { link1, link2 })
+        Dim link2 As New PrintableComponentLink(ps)
+        Chart.DataBind()
+        link2.Component = DirectCast(Chart, IChartContainer).Chart
 
-		compositeLink.CreateDocument()
-		Using stream As New MemoryStream()
-			compositeLink.PrintingSystem.ExportToXls(stream)
-			Response.Clear()
-			Response.Buffer = False
-			Response.AppendHeader("Content-Type", "application/xls")
-			Response.AppendHeader("Content-Transfer-Encoding", "binary")
-			Response.AppendHeader("Content-Disposition", "attachment; filename=test.xls")
-			Response.BinaryWrite(stream.ToArray())
-			Response.End()
-		End Using
-		ps.Dispose()
-	End Sub
+        Dim compositeLink As New CompositeLink(ps)
+        compositeLink.Links.AddRange(New Object() { link1, link2 })
+
+        compositeLink.CreateDocument()
+        Using stream As New MemoryStream()
+            compositeLink.PrintingSystem.ExportToXls(stream)
+            Response.Clear()
+            Response.Buffer = False
+            Response.AppendHeader("Content-Type", "application/xls")
+            Response.AppendHeader("Content-Transfer-Encoding", "binary")
+            Response.AppendHeader("Content-Disposition", "attachment; filename=test.xls")
+            Response.BinaryWrite(stream.ToArray())
+            Response.End()
+        End Using
+        ps.Dispose()
+    End Sub
 End Class
